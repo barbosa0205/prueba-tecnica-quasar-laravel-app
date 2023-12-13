c2fc7bab94ff2c9dba970bf25af949c25d487938
imac <imac@MacBook-Air-de-imac.local>
2023-12-12 18:27:52 -0600

    create Primary Button

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,14 @@
+<template lang="">
+  <q-btn  outline :size="size"  padding="10px 20px" color="primary  " />
+</template>
+<script setup>
+  defineProps({
+    size: {
+      type: String,
+      default: 'lg'
+    }
+  })
+</script>
+<style lang="">
+
+</style>
